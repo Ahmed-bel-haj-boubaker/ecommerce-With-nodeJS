6ee4a4b108a46bc671b0e0a85b48347b8e2225be
Ahmed bel haj boubaker <Ahmed.belhajboubaker@esprit.tn>
2024-02-01 17:00:30 +0100

    add morgan module to log http requests

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
-const express = require('express');
-const dotenv = require('dotenv');
-dotenv.config({path:'config.env'})
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+
+dotenv.config({ path: "config.env" });
 
 const app = express();
-
+if (process.env.NODE_ENV === "development"){
+    app.use(morgan('dev')); // morgan is used for logging HTTP requests like this GET / 200 3.077 ms - 18 ( morgan is a middleware module)
+    console.log(`mode : ${process.env.NODE_ENV}`)
+}
 const PORT = process.env.PORT || 8000;
-app.listen(PORT,()=>{
-    console.log(`Server is listening on port ${PORT} `)
-})
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT} `);
+});
 
-app.get('/',(req,res)=>{
-    res.send('Welcome to express')
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to express");
+});
