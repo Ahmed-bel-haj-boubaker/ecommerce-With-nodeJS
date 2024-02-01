f5f0952365234afab622737ec016923dbc76f2ef
Ahmed bel haj boubaker <Ahmed.belhajboubaker@esprit.tn>
2024-02-01 17:30:02 +0100

    installing Mongoose and connect to our DataBase

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-
+const mongoose = require("mongoose");
 dotenv.config({ path: "config.env" });
+
+//Connect to dataBase
+mongoose.connect(process.env.DB_URI).then(
+    (conn)=>{
+        console.log(`DataBase Connected : ${conn.connection.host}`);
+    }
+).catch(
+    (err)=>{
+        console.log(`error with connecting to dataBase: ${err}`);
+        process.exit(true);
+    }
+)
+
 
 const app = express();
 if (process.env.NODE_ENV === "development"){
