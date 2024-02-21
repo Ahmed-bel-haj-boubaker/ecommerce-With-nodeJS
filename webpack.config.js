6de421a643d5fa664a5b80e7c46115f3935a1abe
Ahmed bel haj boubaker <Ahmed.belhajboubaker@esprit.tn>
2024-02-21 14:57:48 +0100

    build commande

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,16 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'), // Output directory
     filename: 'bundle.js' // Output bundle filename
   },
+  resolve: {
+    fallback: {
+      fs: false, // or require.resolve("stream-browserify")
+      http: false, // or require.resolve("stream-http")
+      net: false, // or require.resolve("net-browserify")
+      url: false, // or require.resolve("url/")
+      buffer: false // or require.resolve("buffer/")
+      // Add other core modules here as needed
+    }
+  },
   module: {
     rules: [
       // Add rules for processing different file types here (e.g., JavaScript, CSS, etc.)
