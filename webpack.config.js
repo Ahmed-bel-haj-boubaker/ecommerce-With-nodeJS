b37b79afab0a3248a6e3601b0cbb3174b88230ab
Ahmed bel haj boubaker <Ahmed.belhajboubaker@esprit.tn>
2024-02-21 14:51:08 +0100

    build commande

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js', // Entry point of your application
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Output directory
+    filename: 'bundle.js' // Output bundle filename
+  },
+  module: {
+    rules: [
+      // Add rules for processing different file types here (e.g., JavaScript, CSS, etc.)
+    ]
+  }
+};
