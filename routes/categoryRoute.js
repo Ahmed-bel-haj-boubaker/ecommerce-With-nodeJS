28e68d5a1f626dbd945af38fbd330a75f147c0ef
Ahmed bel haj boubaker <Ahmed.belhajboubaker@esprit.tn>
2024-03-13 03:35:08 +0100

    part 1 : check if token exist , if exist so get it

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -11,14 +11,14 @@ const {
 const {
   getCategoryByIDValidator, updateCategoryValidator, deleteCategoryValidator, createCategoryValidator,
 } = require("../utils/validator/categoryValidator");
-
+const AuthService = require('../services/authService')
 const subCategoryRoute = require("./subCategoryRoute");
 
 const router = express.Router();
 
 router.use('/:categoryId/subCategory', subCategoryRoute);// if the req is like that /:categoryId/subCategory ==> get me to subCategoryRoute
 
-router.route("/").get(getCategory).post(uploadBrandImage,resizeImage,createCategoryValidator,addCategories);
+router.route("/").get(getCategory).post(AuthService.protect,uploadBrandImage,resizeImage,createCategoryValidator,addCategories);
 router
   .route("/:id")
   .get(getCategoryByIDValidator, getCategoryById)
--- services/authService.js
+++ services/authService.js
@@ -27,7 +27,7 @@ exports.signup = asyncHandler(async (req, res, next) => {
 
   // 2 generate token
 
-  const token = generateToken(user._id)
+  const token = generateToken(user._id);
 
   res.status(201).json({ data: user, token });
 });
@@ -37,12 +37,31 @@ exports.login = asyncHandler(async (req, res, next) => {
   // 2 - check if user exist & if password correct
   const user = await User.findOne({ email: req.body.email });
   if (!user || !(await bcrypt.compare(req.body.password, user.password))) {
-    return next(new ApiError("Incorrect email or password",401));
+    return next(new ApiError("Incorrect email or password", 401));
   }
   // 3 - generate token
 
-  const token = generateToken(user._id)
+  const token = generateToken(user._id);
   // 4 - send Response to client side
 
   res.status(200).json({ data: user, token });
+});
+
+exports.protect = asyncHandler(async (req, res, next) => {
+  // 1 - check if token exist , if exist so get it
+  //console.log(req.headers);
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1]; // to access the token Bearer[0]eyJhbGciOiJIUzI1Ni[1]
+    console.log(token);
+  }
+  if(!token){
+    return next(new ApiError('You are not login , please login to access this route',401));
+  }
+  // 2 - verify token(no change happens , expired token)
+  // 3 - check if user exist from the token ( bcz the id of the user is in the token)
+  // 4 - check if user change his password after token created
 });
