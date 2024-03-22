976c67e142a955cf1c3fd56214e28e74e4290c3c
Ahmed bel haj boubaker <Ahmed.belhajboubaker@esprit.tn>
2024-03-22 01:22:46 +0100

    create Coupon Model

---
+++ models/couponModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const couponSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, "Coupon name required"],
+      unique: true,
+    },
+
+    expire: {
+      type: Date,
+      required: [true, "Coupon expire time required"],
+    },
+    discount: {
+      type: Number,
+      required: [true, "Coupon discount value required"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Coupon',couponSchema);
