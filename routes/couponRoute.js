0a4f3b50cfe4facc6c1e69f09f14ed6be23645ab
Ahmed bel haj boubaker <Ahmed.belhajboubaker@esprit.tn>
2024-03-22 01:43:28 +0100

    complete coupon router

--- routes/couponRoute.js
+++ routes/couponRoute.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const authService = require("../services/authService");
+const couponService = require("../services/couponService");
 
 const router = express.Router();
 
+router.use(authService.protect, authService.allowedTo("user", "manager"));
 
+router
+  .route("/")
+  .get(couponService.getCoupons)
+  .post(couponService.createCoupon);
+router
+  .route("/:id")
+  .get(couponService.getCoupon)
+  .put(couponService.updateCoupon)
+  .delete(couponService.deleteCoupon);
 
 module.exports = router;
